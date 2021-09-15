35064b0d0e66917042d4f8ba62a1021a382404fc
MascarBone <simone.bonelli@gmail.com>
2021-09-15 17:37:56 +0200

    Finalized HTML, CSS and JS

--- js/script.js
+++ js/script.js
@@ -4,32 +4,32 @@ let app = new Vue ({
    data: {
        image: [
         {
-            source: 'https://via.placeholder.com/650x350',
-            alt: 'Placeholder Image'
+            source: 'https://www.taxidrivers.it/wp-content/uploads/2017/02/john-wick-2.jpg',
+            alt: 'Awesome Keanu Rives Image'
         },
         {
-            source: 'https://via.placeholder.com/750x350',
-            alt: 'Placeholder Image'
+            source: 'https://www.pushbutton.it/wp-content/uploads/2020/12/Cyberpunk-2077-10.jpg',
+            alt: 'Awesome Keanu Rives CyberPunk Image'
         },
         {
-            source: 'https://via.placeholder.com/550x350',
-            alt: 'Placeholder Image'
+            source: 'https://www.cinematographe.it/wp-content/uploads/2021/09/matrix-4-114286.jpg',
+            alt: 'Awesome Matrix 4 Image'
         },
         {
-            source: 'https://via.placeholder.com/450x350',
-            alt: 'Placeholder Image'
+            source: 'https://www.gamelegends.it/wp-content/uploads/2021/09/Redfall-970x570.jpg',
+            alt: 'Awesome Redfall Image'
         },
         {
-            source: 'https://via.placeholder.com/150x350',
-            alt: 'Placeholder Image'
+            source: 'https://i.imgur.com/JPwXKHW.gif',
+            alt: 'Awesome Among Us Gif Image'
         },
         {
-            source: 'https://via.placeholder.com/250x350',
-            alt: 'Placeholder Image'
+            source: 'https://qdss.it/wp-content/uploads/2020/04/thumb-1920-942234.jpg',
+            alt: 'Awesome Sekiro Image'
         },
         {
-            source: 'https://via.placeholder.com/350x350',
-            alt: 'Placeholder Image'
+            source: 'https://www.gamelegends.it/wp-content/uploads/2021/01/Death-Stranding.jpg',
+            alt: 'Awesome Death Stranding Image'
         },
     ],    
 
@@ -57,6 +57,9 @@ let app = new Vue ({
                 console.log('logf++')
                 this.imageIndex++;
             }
+        },
+        clickBar : function(index) {
+            this.imageIndex = index;
         }
     }
 });
